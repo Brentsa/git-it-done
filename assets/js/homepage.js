b241a3ae72f86fdaf49b14ee1e48e02b1c5c62ac
Solidshots <seanbrent5@gmail.com>
2021-03-23 08:38:21 -0400

    search functionality operating

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,54 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+var repoContainerEl = document.querySelector("#repos-container");
+
+function handleFormSubmit(event){
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+    if(username){
+        getUserRepos(username);
+    }
+    else{
+        alert("Please enter a valid username.");
+    }
+    
+    userFormEl.reset();
+}
 
 function getUserRepos(user){
-
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     fetch(apiUrl).then(function(response){
         response.json().then(function(data){
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 }
 
-getUserRepos("Brentsa");
+function displayRepos(repos, searchTerm){
+    console.log(repos);
+    console.log(searchTerm);
+
+    repoContainerEl.innerHTML = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    for(var i=0; i<repos.length; i++){
+        //format the repo name
+        var repoName = repos[0].owner.login + "/" + repos[i].name;
+
+        //create container for the repo
+        var repoEl = document.createElement("div");
+        repoEl.className = "list-item flex-row justify-space-between align-center";
+
+        //create span for the repo title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+        
+        repoEl.appendChild(titleEl);
+        repoContainerEl.appendChild(repoEl);
+    }
+}
+
+userFormEl.addEventListener("submit", handleFormSubmit);
